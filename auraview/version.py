7c1b36f56d7148403c327aced469bb0a9a6cc8da
Benevant Mathew @build3 <benevantmathewv@gmail.com>
2026-02-23 02:01:55 +0530

    released v0.2.0

--- auraview/version.py
+++ auraview/version.py
@@ -2,12 +2,12 @@
 version.py
 
 Author: Benevant Mathew
-Date: 2026-02-19
+Date: 2026-02-23
 """
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Benevant Mathew"
 __email__ = "benevantmathewv@gmail.com"
-__release_date__="19-02-2026"
+__release_date__="23-02-2026"
 
 if __name__ == "__main__":
     print(__version__)
